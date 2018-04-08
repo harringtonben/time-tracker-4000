ec630a1a7917810b118d6879eebd0ef71235ce84
Ben Harrington <harringtonben@icloud.com>
2018-04-08 15:21:44 -0500

    finished working through first post, now testing

---
+++ TimeTracker/Controllers/ShiftsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TimeTracker.Services;
+
+namespace TimeTracker.Controllers
+{
+    [RoutePrefix("api/shifts")]
+    public class ShiftsController : ApiController
+    {
+        [HttpPost, Route("")]
+        public HttpResponseMessage Create(ShiftDto shift)
+        {
+            var shiftRepository = new ShiftRepository();
+            var shiftToInsert = new Shift
+            {
+                EmployeeId = shift.EmployeeId,
+                ManagerId = shift.ManagerId,
+                WorkFromHome = shift.WorkFromHome,
+                Callout = shift.Callout,
+                Planned = shift.Planned,
+                ShiftLengthId = shift.ShiftLengthId,
+                Email = shift.Email,
+                Phone = shift.Phone,
+                Integrations = shift.Integrations,
+                NonCoverage = shift.NonCoverage
+            };
+
+            var addShift = shiftRepository.AddShift(shiftToInsert);
+
+            if (addShift)
+                return Request.CreateResponse(HttpStatusCode.Created);
+
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Unable to add shift. Please try again later.");
+        }
+    }
+
+    public class ShiftDto
+    {
+        public int EmployeeId { get; set; }
+        public int ManagerId { get; set; }
+        public bool WorkFromHome { get; set; }
+        public bool Callout { get; set; }
+        public bool Planned { get; set; }
+        public int ShiftLengthId { get; set; }
+        public bool Email { get; set; }
+        public bool Phone { get; set; }
+        public bool Integrations { get; set; }
+        public bool NonCoverage { get; set; }
+    }
+}
---
+++ TimeTracker/Services/ShiftRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracker.Services
+{
+    public class ShiftRepository
+    {
+        public bool AddShift(Shift shift)
+        {
+            using (var db = createConnection())
+            {
+                db.Open();
+
+                var newShift = db.Execute(@"INSERT INTO Shifts
+                                                       (EmployeeId
+                                                       ,ManagerId
+                                                       ,WorkFromHome
+                                                       ,Callout
+                                                       ,Planned
+                                                       ,ShiftLengthId
+                                                       ,Email
+                                                       ,Phone
+                                                       ,Integrations
+                                                       ,NonCoverage)
+                                                 VALUES
+                                                       (
+                                                        @employeeId
+                                                       ,@managerId
+                                                       ,@workFromHome
+                                                       ,@callout
+                                                       ,@planned
+                                                       ,@shiftLengthId
+                                                       ,@email
+                                                       ,@phone
+                                                       ,@integrations
+                                                       ,@nonCoverage)", shift);
+
+                return newShift == 1;
+            }
+        }
+
+        private SqlConnection createConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString);
+        }
+    }
+
+    public class Shift
+    {
+        public int EmployeeId { get; set; }
+        public int ManagerId { get; set; }
+        public bool WorkFromHome { get; set; }
+        public bool Callout { get; set; }
+        public bool Planned { get; set; }
+        public int ShiftLengthId { get; set; }
+        public bool Email { get; set; }
+        public bool Phone { get; set; }
+        public bool Integrations { get; set; }
+        public bool NonCoverage { get; set; }
+    }
+}
