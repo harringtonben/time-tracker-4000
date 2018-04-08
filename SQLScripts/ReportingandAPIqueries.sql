c86ca28436a04862a894f568877b7513251e7173
Ben Harrington <harringtonben@icloud.com>
2018-04-08 14:19:31 -0500

    working through finishing sql queries

--- SQLScripts/ReportingandAPIqueries.sql
+++ SQLScripts/ReportingandAPIqueries.sql
@@ -44,8 +44,33 @@ where s.MangerId = 1
 and date > getdate() - 28
 
 --Select percentage of unplanned days vs. planned days for a supporter within x amount of weeks
+with callout_cte (sickdays, employeeid) as
+(
+select count(*) as sickdays, EmployeeId from shifts
+where EmployeeId = 12
+and date > getdate() - 33
+and Callout = 1
+and Planned = 0
+group by EmployeeId
+),
+allshift_cte (allworkdays, employeeid) as
+(
+select count(*) as allworkdays, EmployeeId from Shifts
+where EmployeeId = 12
+and date > getdate() - 33
+group by EmployeeId
+)
+select distinct (c.sickdays * 100)/a.allworkdays as unplannedsicktime from shifts s
+join callout_cte c on c.employeeid = s.EmployeeId
+join allshift_cte a on a.employeeid = s.EmployeeId
 
 --Select all Email days for a certain supporter
+select s.*, e.name from Shifts s
+join Employees e on e.employeeid = s.employeeid
+where date > getdate() - 28
+and s.EmployeeId = 12
+and Email =1
+
 
 --Select all phone days for a certain supporter
 
